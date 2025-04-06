decb45cb318f4ae5c5ff7ba76c88db5b83c78fdc
pearlrhema <pearlrhema8@gmail.com>
2025-04-06 12:48:58 +0100

    registration and login view working

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -17,6 +17,20 @@ accountCon.buildLogin = async function buildLogin(req, res, next) {
     }
 }
 
+/* ****************************************
+*  Deliver registration view
+* *************************************** */
+accountCon.buildRegister = async function buildRegister(req, res, next) {
+    let nav = await utilities.getNav()
+    res.render("account/register", {
+      title: "Register",
+      nav,
+      errors: null
+    })
+  }
+
+
+
 module.exports = accountCon;
 
 /* after this you build the login view which is what will display when the user clicks on the my account link */
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,6 +5,7 @@ const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/");
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 module.exports = router;
 
