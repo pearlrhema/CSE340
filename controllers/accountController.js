b636efbcf07dec6dbfd5b22afbf79cdf2c72aca7
pearlrhema <pearlrhema8@gmail.com>
2025-04-09 12:25:36 +0100

    password hashing

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,8 @@
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
 
+//the password validation starts here by adding bcryptjs to the dependencies ( pnpm add bcrytjs)
+const bcrypt = require("bcryptjs")
 
 const accountCon = {}
 
@@ -34,13 +36,27 @@ accountCon.buildRegister = async function buildRegister(req, res, next) {
 * *************************************** */
 accountCon.registerAccount = async function registerAccount(req, res) {
     let nav = await utilities.getNav()
-    const { account_firstname, account_lastname, account_email, account_password } = req.body
+  const { account_firstname, account_lastname, account_email, account_password } = req.body
+  
+  // (next on password validation) Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
   
     const regResult = await accountModel.registerAccount(
       account_firstname,
       account_lastname,
       account_email,
-      account_password
+      hashedPassword
     )
   
     if (regResult) {
