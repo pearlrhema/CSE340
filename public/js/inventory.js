275f78fe050558148ec5bfd59dc5cb865dc07b20
pearlrhema <pearlrhema8@gmail.com>
2025-04-13 18:23:02 +0100

    function for delete action confirmation added

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -23,6 +23,27 @@ classificationList.addEventListener("change", function () {
 })
 //this function requests the data, based on the classification_ID and catches any errors if they exist, and sends the retrieved data to the buildInventoryList function for building it into HTML and then displays it into the management view
 
+//THIS IS THE FUNCTION THAT BUILDS THE HTML TABLE WITHOUT BUTTON
+// Build inventory items into HTML table components and inject into DOM 
+// function buildInventoryList(data) { 
+//   let inventoryDisplay = document.getElementById("inventoryDisplay"); 
+//   // Set up the table labels 
+//   let dataTable = '<thead>'; 
+//   dataTable += '<tr><th>Vehicle Name</th><td>&nbsp;</td><td>&nbsp;</td></tr>'; 
+//   dataTable += '</thead>'; 
+//   // Set up the table body 
+//   dataTable += '<tbody>'; 
+//   // Iterate over all vehicles in the array and put each in a row 
+//   data.forEach(function (element) { 
+//    console.log(element.inv_id + ", " + element.inv_model); 
+//    dataTable += `<tr><td>${element.inv_make} ${element.inv_model}</td>`; 
+//    dataTable += `<td><a href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>`; 
+//    dataTable += `<td><a href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td></tr>`; 
+//   }) 
+//   dataTable += '</tbody>'; 
+//   // Display the contents in the Inventory Management view 
+//   inventoryDisplay.innerHTML = dataTable; 
+//  }
 
 // Build inventory items into HTML table components and inject into DOM 
 function buildInventoryList(data) {
@@ -65,7 +86,14 @@ function buildInventoryList(data) {
   
     // Display the contents in the Inventory Management view
     inventoryDisplay.innerHTML = dataTable;
+}
+
+//THIS FUNCTION CONFIRMS THE DELETE ACTION
+function confirmDelete(id) {
+  if (confirm('Are you sure you want to delete this vehicle?')) {
+    window.location.href = '/inv/delete/' + id;
   }
-  
+}
+
    
 //next we edit the inventory route
